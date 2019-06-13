b2f18f7f53d1678d7bd419417f2554e3701e78a0
humm3r1 <6620937+humm3r1@users.noreply.github.com>
2019-06-13 14:21:08 -0400

    Update build_chroot.sh

--- build/build_chroot.sh
+++ build/build_chroot.sh
@@ -9,12 +9,13 @@ export HOME=/root
 export LC_ALL=C
 
 #Setup our apt repos to include universe for nvme-cli, python3-dialog
-echo "deb http://archive.ubuntu.com/ubuntu bionic main universe" > /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian stretch main contrib non-free" > /etc/apt/sources.list
 
 #Install requirements
 export DEBIAN_FRONTEND=noninteractive 
 apt-get update -y
 apt-get install hdparm pm-utils python3 python3-dialog nvme-cli python3-yaml linux-image-generic linux-firmware dbus casper lupin-casper discover laptop-detect ubuntu-standard linux-generic live-boot live-boot-initramfs-tools live-tools extlinux -y 
+apt-get install initramfs-tools linux-headers-amd64 linux-image-4.9.0-9-amd64 -y
 
 #Items we use just in the wrapper script
 apt-get install curl dmidecode dstat chntpw -y 
@@ -29,6 +30,7 @@ update-initramfs -u
 
 #Hush the motd
 touch /root/.hushlogin
+mkdir /home/monitoring
 touch /home/monitoring/.hushlogin
 
 #Clean up
