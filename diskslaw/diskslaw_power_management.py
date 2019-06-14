4a7487cf381dac7bdb63ddbb12bd1ae35d7bb300
humm3r1 <6620937+humm3r1@users.noreply.github.com>
2019-06-14 09:38:11 -0400

    Update diskslaw_power_management.py
    Trying out rtcwake instead of pm-suspend to auto resume

--- diskslaw/diskslaw_power_management.py
+++ diskslaw/diskslaw_power_management.py
@@ -6,5 +6,5 @@ def disable_terminal_blanking():
 
 #Wrapper around pm-suspend to put computer to sleep
 def suspend_computer():
-    proc = Popen(['pm-suspend'])
+    proc = Popen(['rtcwake -s 5'])
     proc.communicate()
